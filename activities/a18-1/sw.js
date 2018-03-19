9bc9d5e7e864d76c041a82a7a84ae8aea27f1a38
Dylan Vo <dvo7@uic.edu>
2018-03-19 22:12:10 +0000

    Fetch feature added Commit

--- activities/a18-1/sw.js
+++ activities/a18-1/sw.js
@@ -1,3 +1,23 @@
-self.addEventListener('install', function(event) {
-  console.log(event);
+self.addEventListener('install', function(e) {
+  console.log('Install',e);
+  e.waitUntil(
+    caches.open("cache-simple").then(function(cache) {
+      console.log('[ServiceWorker] Caching index.html');
+      return cache.add("index.html");
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  console.log('activate',event);
+});
+
+self.addEventListener('fetch', function(e) {
+    // log the event
+    console.log('[Service Worker] Fetch', e);
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
 });
